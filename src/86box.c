309faf59b710485ad117c3d756385b0e05645214
cold-brewed <47337035+cold-brewed@users.noreply.github.com>
2024-05-09 07:20:37 -0400

    User-defined keyboard shortcut POC

--- src/86box.c
+++ src/86box.c
@@ -208,6 +208,7 @@ int      do_auto_pause                          = 0;              /* (C) Auto-pa
                                                                          loss */
 int      hook_enabled                           = 1;              /* (C) Keyboard hook is enabled */
 char     uuid[MAX_UUID_LEN]                     = { '\0' };       /* (C) UUID or machine identifier */
+char     release_shortcut[MAX_KEYBOARD_SHORTCUT_LEN] = { '\0' };  /* (C) Portable text representation of the keyboard shortcut */
 
 int      other_ide_present = 0;                                   /* IDE controllers from non-IDE cards are
                                                                      present */
