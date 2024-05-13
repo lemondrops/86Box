ca0311f40d2e735ce7921b367cc988470adbd543
cold-brewed <47337035+cold-brewed@users.noreply.github.com>
2024-05-13 11:25:09 -0400

    hdd presets, all-new hdd dialog

--- src/qt/qt_harddiskdialog.hpp
+++ src/qt/qt_harddiskdialog.hpp
@@ -2,6 +2,10 @@
 #define QT_HARDDISKDIALOG_HPP
 
 #include <QDialog>
+#include <QSortFilterProxyModel>
+#include <QStandardItemModel>
+#include <QStringListModel>
+#include <optional>
 
 namespace Ui {
 class HarddiskDialog;
@@ -22,6 +26,17 @@ public:
     uint32_t sectors() const { return sectors_; }
     uint32_t speed() const;
 
+    struct HDDPreset {
+        int     id;
+        QString manufacturer;
+        QString model;
+        int     size;
+        int     c;
+        int     h;
+        int     s;
+        QString bus;
+    };
+
 signals:
     void fileProgress(int i);
 
@@ -29,7 +44,6 @@ public slots:
     void accept() override;
 
 private slots:
-    void on_comboBoxType_currentIndexChanged(int index);
     void on_lineEditSectors_textEdited(const QString &arg1);
     void on_lineEditHeads_textEdited(const QString &arg1);
     void on_lineEditCylinders_textEdited(const QString &arg1);
@@ -38,13 +52,29 @@ private slots:
     void on_comboBoxFormat_currentIndexChanged(int index);
     void onCreateNewFile();
     void onExistingFileSelected(const QString &fileName, bool precheck);
+    void populatePresetModels(int index) const;
+    void setPresetParameters(int index);
+    void presetTextChanged(const QString &text);
+    void sizeSliderChanged(int value);
+    void sizeSliderMoved(int value) const;
+    void sizeSliderRangeChanged(int min, int max) const;
 
 private:
+    enum HDDPresetModelRoles {
+        Manufacturer = Qt::UserRole + 1,
+        Model,
+        Size,
+        Cylinders,
+        Heads,
+        Sectors,
+        Bus,
+    };
+
     Ui::HarddiskDialog *ui;
 
-    uint32_t cylinders_;
-    uint32_t heads_;
-    uint32_t sectors_;
+    uint32_t cylinders_ = 0;
+    uint32_t heads_ = 0;
+    uint32_t sectors_ = 0;
 
     uint32_t max_sectors   = 0;
     uint32_t max_heads     = 0;
@@ -55,13 +85,22 @@ private:
     QStringList filters;
     // "Dynamic-size VHD" is number 4 in the `filters` list and the
     // comboBoxFormat model
-    const uint8_t DEFAULT_DISK_FORMAT = 4;
+    const uint8_t  DEFAULT_DISK_FORMAT     = 4;
+    const uint16_t DEFAULT_MAX_SIZE_SLIDER = 4096;
 
     bool checkAndAdjustCylinders();
     bool checkAndAdjustHeads();
     bool checkAndAdjustSectors();
     void recalcSize();
     void recalcSelection();
+    void parsePresetList();
+    static std::optional<HDDPreset> presetFromJson(const QJsonObject& json);
+    QStringListModel*      createManufacturersModel();
+    QStandardItemModel*    createHddPresetModel();
+    QSortFilterProxyModel* createHddPresetProxyModel(QStandardItemModel* sourceModel);
+
+    QList<HDDPreset>       presets;
+    QSortFilterProxyModel *hddPresetModel;
 };
 
 #endif // QT_HARDDISKDIALOG_HPP
