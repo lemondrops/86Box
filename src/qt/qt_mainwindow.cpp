088529209d1ed6b68b0b0c6e0d439237509e5c5d
cold-brewed <47337035+cold-brewed@users.noreply.github.com>
2024-06-08 13:25:09 -0400

    vmm preview

--- src/qt/qt_mainwindow.cpp
+++ src/qt/qt_mainwindow.cpp
@@ -2148,6 +2148,7 @@ MainWindow::updateUiPauseState()
                                     QString(tr("Pause execution"));
     ui->actionPause->setIcon(pause_icon);
     ui->actionPause->setToolTip(tooltip_text);
+    emit vmmRunningStateChanged(static_cast<VMManagerProtocol::RunningState>(dopause));
 }
 
 void
